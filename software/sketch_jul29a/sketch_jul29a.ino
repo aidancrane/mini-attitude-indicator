be828e648d32ff1295dc192072fc17d51e00d3c1
Aidan Crane <aidancrane78@gmail.com>
2023-07-30 12:46:54 +0100

    Update sketch_jul29a.ino
    Added sample displayRandomEulerAngles

--- software/sketch_jul29a/sketch_jul29a.ino
+++ software/sketch_jul29a/sketch_jul29a.ino
@@ -15,6 +15,9 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
   OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
+
+int COUNT = 0; //res 10
+
 void setup() {
 
   Serial.begin(9600);
@@ -38,13 +41,56 @@ void setup() {
   display.setCursor(0, 0);
 
   // Print "Hello World" to the display
-  display.println(F("Hello World"));
+  display.println(F("Hello World 123"));
   Serial.println(F("Hello World"));
 
-  // Display the content on the screen
-  display.display();
+  // Draw a ball
+  int ballCenterX = SCREEN_WIDTH / 2;  // X coordinate of the ball's center
+  int ballCenterY = SCREEN_HEIGHT / 2; // Y coordinate of the ball's center
+  int ballRadius = 10;                 // Radius of the ball in pixels
+  display.drawCircle(ballCenterX, ballCenterY, ballRadius, SSD1306_WHITE);
+
+
 }
 
 void loop() {
-  // Nothing to do here for this example
+  
+  // Clear the buffer
+  display.clearDisplay();
+
+   // Generate random Euler angles and display them on the screen
+  displayRandomEulerAngles();
+
+  // Display the content on the screen
+  display.display();
+
+}
+
+void displayRandomEulerAngles() {
+  float roll = random(-90, 91);  // Generate random roll angle between -90 and 90 degrees
+  float pitch = random(-90, 91); // Generate random pitch angle between -90 and 90 degrees
+  float yaw = random(0, 361);    // Generate random yaw angle between 0 and 360 degrees
+
+  // // Clear the previous text by drawing a filled rectangle over the area
+  // display.fillRect(0, 24, SCREEN_WIDTH, 24, SSD1306_BLACK);
+
+  // Set cursor position to (0, 24) for displaying Euler angles
+  display.setCursor(0, 24);
+
+  // Print the Euler angles to the display
+  display.print(F("Roll: "));
+  display.println(roll);
+
+  display.print(F("Pitch: "));
+  display.println(pitch);
+
+  display.print(F("Yaw: "));
+  display.println(yaw);
+
+  display.print(F("Count: "));
+  display.println(COUNT);
+  COUNT++;
+
+  // Delay
+  delay(500);
 }
